c21459488675be85fa92199dbfad62255f7dcc28
Mufeez Qadri <mufeez.qadri22@gmail.com>
2025-12-18 21:38:41 +0530

    Initial Commit

---
+++ MediaOrganizer/MediaOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaOrganizerApp.swift
+//  MediaOrganizer
+//
+//  Created by Mufeeez . on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MediaOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
